df5acf3b523ff040e7c1b83ddb9520ff7007e523
jsv-assignment <166288218+jsv-assignment@users.noreply.github.com>
2024-04-07 15:06:58 +0530

    Update train.py

--- train.py
+++ train.py
@@ -15,4 +15,4 @@ model = LogisticRegression().fit(X, y)
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
 
-print('done11111111111122222222223333333333')
+print('done1111111111112222222222333333333344444444444')
